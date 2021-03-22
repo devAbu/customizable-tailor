64534087385180926bce91d90a2d6ac941b3fd9c
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2021-03-22 20:28:38 +0100

    Reset everythin

--- dist/index.js
+++ dist/index.js
@@ -2,22 +2,49 @@
 let categories = document.querySelector('.categories');
 let options_body = document.querySelector('.options-body');
 let total_price = document.querySelector('.price');
+let reset_button = document.querySelector('.btn-reset');
+let elems = document.querySelectorAll('.category-container');
+let hiddenElems = document.querySelectorAll('.category-options');
+let options_box = document.querySelectorAll('.options-content');
 let main_image = document.querySelector('.main-image img');
 localStorage.setItem('type', 'man');
 localStorage.setItem('sleeve', 'long');
 localStorage.setItem('collar', 'business classic');
 localStorage.setItem('cuff', 'single button');
 localStorage.setItem('material', 'white');
-fetch('./php/getImage.php?type=man&material=white&sleeve=long&collar=business classic&cuff=single button').then(function (response) {
-    return response.blob();
-}).then(function (myBlob) {
-    var objectURL = URL.createObjectURL(myBlob);
-    main_image.src = objectURL;
+let firstImage = () => {
+    fetch('./php/getImage.php?type=man&material=white&sleeve=long&collar=business classic&cuff=single button').then(function (response) {
+        return response.blob();
+    }).then(function (myBlob) {
+        var objectURL = URL.createObjectURL(myBlob);
+        main_image.src = objectURL;
+        main_image.classList.remove('hidden');
+    });
+};
+firstImage();
+reset_button.addEventListener('click', () => {
+    firstImage();
+    for (let i = 0; i < elems.length; i++) {
+        elems[i].classList.remove('selected');
+    }
+    for (let i = 0; i < hiddenElems.length; i++) {
+        hiddenElems[i].classList.add('hidden');
+    }
+    for (let i = 0; i < options_box.length; i++) {
+        if (options_box[i].classList.contains('man') || options_box[i].classList.contains('white') || options_box[i].classList.contains('long') || options_box[i].classList.contains('businessClassic') || options_box[i].classList.contains('singleButton')) {
+            options_box[i].classList.add('active');
+        }
+        else {
+            options_box[i].classList.remove('active');
+        }
+    }
+    localStorage.setItem('type', 'man');
+    localStorage.setItem('sleeve', 'long');
+    localStorage.setItem('collar', 'business classic');
+    localStorage.setItem('cuff', 'single button');
+    localStorage.setItem('material', 'white');
 });
 categories.addEventListener('click', (e) => {
-    console.log('juhu');
-    let elems = document.querySelectorAll('.category-container');
-    let hiddenElems = document.querySelectorAll('.category-options');
     for (let i = 0; i < elems.length; i++) {
         elems[i].classList.remove('selected');
     }
@@ -89,7 +116,6 @@ categories.addEventListener('click', (e) => {
 let closeButtons = document.getElementsByClassName('close-options');
 Array.prototype.forEach.call(closeButtons, (element) => {
     element.addEventListener('click', (e) => {
-        console.log('Close Model -- X');
         e.target.parentNode.parentNode.classList.add('hidden');
     });
 });
@@ -110,88 +136,88 @@ Array.prototype.forEach.call(options, (element) => {
         let change = document.querySelector('.change');
         let doubleButton = document.querySelector('.doubleButton');
         let envelope = document.querySelector('.envelope');
-        if (e.target.classList.contains('woman')) {
+        if (e.target.classList.contains('woman') || e.target.parentNode.classList.contains('woman')) {
             man.classList.remove('active');
             woman.classList.add('active');
             localStorage.setItem("type", "woman");
         }
-        else if (e.target.classList.contains('man')) {
+        else if (e.target.classList.contains('man') || e.target.parentNode.classList.contains('man')) {
             woman.classList.remove('active');
             man.classList.add('active');
             localStorage.setItem("type", "man");
         }
-        if (e.target.classList.contains('white')) {
+        if (e.target.classList.contains('white') || e.target.parentNode.classList.contains('white')) {
             lugaBlue.classList.remove('active');
             purgaBlackBlue.classList.remove('active');
             white.classList.add('active');
             localStorage.setItem("material", "white");
         }
-        else if (e.target.classList.contains('luga-blue')) {
+        else if (e.target.classList.contains('luga-blue') || e.target.parentNode.classList.contains('luga-blue')) {
             lugaBlue.classList.add('active');
             purgaBlackBlue.classList.remove('active');
             white.classList.remove('active');
             localStorage.setItem("material", "luga-blue");
         }
-        else if (e.target.classList.contains('parga-black-blue')) {
+        else if (e.target.classList.contains('parga-black-blue') || e.target.parentNode.classList.contains('parga-black-blue')) {
             lugaBlue.classList.remove('active');
             purgaBlackBlue.classList.add('active');
             white.classList.remove('active');
             localStorage.setItem("material", "parga-black-blue");
         }
-        if (e.target.classList.contains('short')) {
+        if (e.target.classList.contains('short') || e.target.parentNode.classList.contains('short')) {
             long.classList.remove('active');
             short.classList.add('active');
             localStorage.setItem("sleeve", "short");
             document.querySelector('.cuff').style.pointerEvents = 'none';
             document.querySelector('.cuff').style.opacity = 0.5;
         }
-        else if (e.target.classList.contains('long')) {
+        else if (e.target.classList.contains('long') || e.target.parentNode.classList.contains('long')) {
             short.classList.remove('active');
             long.classList.add('active');
             localStorage.setItem("sleeve", "long");
             document.querySelector('.cuff').style.pointerEvents = 'auto';
             document.querySelector('.cuff').style.opacity = 1;
         }
-        if (e.target.classList.contains('businessClassic')) {
+        if (e.target.classList.contains('businessClassic') || e.target.parentNode.classList.contains('businessClassic')) {
             businessClassic.classList.add('active');
             cut_awayClassic.classList.remove('active');
             cut_awayModern.classList.remove('active');
             localStorage.setItem("collar", "business classic");
         }
-        else if (e.target.classList.contains('cut-awayClassic')) {
+        else if (e.target.classList.contains('cut-awayClassic') || e.target.parentNode.classList.contains('cut-awayClassic')) {
             businessClassic.classList.remove('active');
             cut_awayClassic.classList.add('active');
             cut_awayModern.classList.remove('active');
             localStorage.setItem("collar", "cut-away classic");
         }
-        else if (e.target.classList.contains('cut-awayModern')) {
+        else if (e.target.classList.contains('cut-awayModern') || e.target.parentNode.classList.contains('cut-awayModern')) {
             businessClassic.classList.remove('active');
             cut_awayClassic.classList.remove('active');
             cut_awayModern.classList.add('active');
             localStorage.setItem("collar", "cut-away modern");
         }
-        if (e.target.classList.contains('singleButton')) {
+        if (e.target.classList.contains('singleButton') || e.target.parentNode.classList.contains('singleButton')) {
             singleButton.classList.add('active');
             change.classList.remove('active');
             doubleButton.classList.remove('active');
             envelope.classList.remove('active');
             localStorage.setItem("cuff", "single button");
         }
-        else if (e.target.classList.contains('change')) {
+        else if (e.target.classList.contains('change') || e.target.parentNode.classList.contains('change')) {
             singleButton.classList.remove('active');
             change.classList.add('active');
             doubleButton.classList.remove('active');
             envelope.classList.remove('active');
             localStorage.setItem("cuff", "change");
         }
-        else if (e.target.classList.contains('doubleButton')) {
+        else if (e.target.classList.contains('doubleButton') || e.target.parentNode.classList.contains('doubleButton')) {
             singleButton.classList.remove('active');
             change.classList.remove('active');
             doubleButton.classList.add('active');
             envelope.classList.remove('active');
             localStorage.setItem("cuff", "double button");
         }
-        else if (e.target.classList.contains('envelope')) {
+        else if (e.target.classList.contains('envelope') || e.target.parentNode.classList.contains('envelope')) {
             singleButton.classList.remove('active');
             change.classList.remove('active');
             doubleButton.classList.remove('active');
